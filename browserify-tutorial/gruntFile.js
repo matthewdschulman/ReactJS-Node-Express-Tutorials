ed93e7d86a595cbeeb827def3da2cc8b4d97cd45
Matt Schulman <matthewdschulman@gmail.com>
2015-12-25 13:14:10 -0500

    finish browserify tutorial sans gulp. will use grunt because it is what facebook recommends in its react readme

---
+++ browserify-tutorial/gruntFile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['browserify', 'watch']);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      main: {
+		options: {
+		  bundleOptions: {
+		  	debug: true
+		  }
+		},
+        src: 'js/main.js',
+        dest: 'js/findem.js'
+      }
+    },
+    watch: {
+      files: 'js/*',
+      tasks: ['default']
+    }
+  });
+}
